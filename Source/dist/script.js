9e88e2ea5f503765eea56853782ad3993ed0b1b1
Amir Latipov <kimciwork@gmail.com>
2021-09-08 18:52:15 +0300

    modal modif

--- Source/dist/script.js
+++ Source/dist/script.js
@@ -955,44 +955,87 @@ __webpack_require__.r(__webpack_exports__);
 
 var modal = function modal() {
   function bindModal(openTrigger, Modalselector, closeTrigger) {
-    var BgOverlayClick = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : true;
+    var destroy = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
     var open = document.querySelectorAll(openTrigger),
         modal = document.querySelector(Modalselector),
         close = document.querySelector(closeTrigger),
         popups = document.querySelectorAll('[data-modal]'),
         scrollWidth = hideScroll();
+    var checkBtn = 0;
     open.forEach(function (item) {
       item.addEventListener('click', function (e) {
+        checkBtn = 1;
+        console.log(checkBtn);
+
         if (e.target) {
           e.preventDefault();
         }
 
+        if (destroy == true) {
+          item.style.display = "none";
+        }
+
+        popups.forEach(function (item) {
+          item.style.display = 'none';
+          item.classList.add('animated', 'fadeIn');
+        });
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
         document.body.style.marginRight = "".concat(scrollWidth, "px");
+
+        if (modal.style.display == 'block') {
+          document.querySelector('.fixed-gift').style.marginRight = "".concat(scrollWidth, "px");
+        } else {
+          return;
+        }
       });
     });
     close.addEventListener('click', function () {
       modal.style.display = 'none';
       document.body.style.overflow = '';
       document.body.style.marginRight = "0px";
+
+      if (modal.style.display == 'none') {
+        document.querySelector('.fixed-gift').style.marginRight = "0px";
+      }
+
       popups.forEach(function (item) {
         item.style.display = 'none';
       });
     });
     modal.addEventListener('click', function (e) {
-      if (e.target === modal && BgOverlayClick) {
+      if (e.target === modal) {
         modal.style.display = 'none';
         document.body.style.overflow = '';
         document.body.style.marginRight = "0px";
+
+        if (modal.style.display == 'none') {
+          document.querySelector('.fixed-gift').style.marginRight = "0px";
+        }
+
         popups.forEach(function (item) {
           item.style.display = 'none';
         });
       }
     });
+    console.log(checkBtn);
+
+    function endPageModal(selector) {
+      window.addEventListener('scroll', function () {
+        var allScrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+        var scrollingHeight = window.pageYOffset + document.documentElement.clientHeight;
+
+        if (checkBtn != 1 && scrollingHeight == allScrollHeight) {
+          document.querySelector(selector).click();
+        }
+      });
+    }
+
+    endPageModal('.fixed-gift');
   }
 
   function openModalTimer(selector, time) {
+    var sw = hideScroll();
     setTimeout(function () {
       var displayStatus;
       document.querySelectorAll('[data-modal]').forEach(function (item) {
@@ -1004,6 +1047,7 @@ var modal = function modal() {
       if (displayStatus !== "true") {
         document.querySelector(selector).style.display = 'block';
         document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = "".concat(sw, "px");
       }
     }, time);
   }
@@ -1022,7 +1066,7 @@ var modal = function modal() {
 
   bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
   bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
-  openModalTimer('.popup-consultation', 60000);
+  bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true); // openModalTimer('.popup-consultation', 60000);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (modal);
--- Source/src/js/modules/modals.js
+++ Source/src/js/modules/modals.js
@@ -1,21 +1,36 @@
 const modal = () => {
-  function bindModal(openTrigger, Modalselector, closeTrigger, BgOverlayClick = true) {
+  function bindModal(openTrigger, Modalselector, closeTrigger, destroy = false) {
     const open = document.querySelectorAll(openTrigger),
       modal = document.querySelector(Modalselector),
       close = document.querySelector(closeTrigger),
       popups = document.querySelectorAll('[data-modal]'),
       scrollWidth = hideScroll();
-
+    let checkBtn = 0;
 
     open.forEach((item) => {
       item.addEventListener('click', (e) => {
+        checkBtn = 1;
+        console.log(checkBtn);
         if (e.target) {
           e.preventDefault();
         }
 
+        if (destroy == true) {
+          item.style.display = "none";
+        }
+
+        popups.forEach(item => {
+          item.style.display = 'none';
+          item.classList.add('animated', 'fadeIn');
+        });
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
         document.body.style.marginRight = `${scrollWidth}px`;
+        if (modal.style.display == 'block') {
+          document.querySelector('.fixed-gift').style.marginRight = `${scrollWidth}px`;
+        } else {
+          return;
+        }
       });
     });
 
@@ -23,24 +38,46 @@ const modal = () => {
       modal.style.display = 'none';
       document.body.style.overflow = '';
       document.body.style.marginRight = `0px`;
+      if (modal.style.display == 'none') {
+        document.querySelector('.fixed-gift').style.marginRight = `0px`;
+      }
       popups.forEach((item) => {
         item.style.display = 'none';
       });
     });
 
     modal.addEventListener('click', (e) => {
-      if (e.target === modal && BgOverlayClick) {
+      if (e.target === modal) {
         modal.style.display = 'none';
         document.body.style.overflow = '';
         document.body.style.marginRight = `0px`;
+        if (modal.style.display == 'none') {
+          document.querySelector('.fixed-gift').style.marginRight = `0px`;
+        }
         popups.forEach((item) => {
           item.style.display = 'none';
         });
       }
     });
+    console.log(checkBtn);
+
+    function endPageModal(selector) {
+      window.addEventListener('scroll', () => {
+        let allScrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+        let scrollingHeight = window.pageYOffset + document.documentElement.clientHeight;
+
+        if (checkBtn != 1 && (scrollingHeight == allScrollHeight)) {
+          document.querySelector(selector).click();
+        }
+      });
+
+    }
+
+    endPageModal('.fixed-gift');
   }
 
   function openModalTimer(selector, time) {
+    let sw = hideScroll();
     setTimeout(function () {
       let displayStatus;
 
@@ -53,6 +90,7 @@ const modal = () => {
       if (displayStatus !== "true") {
         document.querySelector(selector).style.display = 'block';
         document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${sw}px`;
       }
     }, time);
   }
@@ -73,7 +111,8 @@ const modal = () => {
 
   bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
   bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
-  openModalTimer('.popup-consultation', 60000);
+  bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+  // openModalTimer('.popup-consultation', 60000);
 };
 
 export default modal;
